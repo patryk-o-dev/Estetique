9f987af26cd995ddaf73137ed0cced4102db41d3
patryk-o-dev <patryk.o.dev@gmail.com>
2026-01-18 19:28:36 +0100

    style: ServicesPricing

--- src/components/layout/ServicesPricing/ServicesPricing.tsx
+++ src/components/layout/ServicesPricing/ServicesPricing.tsx
@@ -1,71 +1,67 @@
 import ButtonArrow from "../../utils/ButtonArrow/ButtonArrow";
 import ButtonMain from "../../utils/ButtonMain/ButtonMain";
+import imgBack from "../../../assets/images/servicesPricing/servicesPricingBack.png";
+import imgFront from "../../../assets/images/servicesPricing/servicesPricingFront.png";
+import servicesBackground from "../../../assets/images/servicesPricing/servicesBackground.png";
+import styles from "./ServicesPricing.module.scss";
 
 const ServicesPricing = () => {
+	const services = [
+		{
+			title: "Face Treatments",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			price: 50.0,
+		},
+		{
+			title: "Facial & Massage",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			price: 90.0,
+		},
+		{
+			title: "Hair & Treatment",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			price: 95.0,
+		},
+		{
+			title: "Face Treatments",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			price: 95.0,
+		},
+		{
+			title: "Manicure & Pedicure",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			price: 70.0,
+		},
+	];
 	return (
-		<section>
-			<div>
-				<img src="" alt="" />
-				<img src="" alt="" />
+		<section className={styles.servicesPricing}>
+			<div className={styles.images}>
+				<img src={imgBack} alt="Background" className={styles.imgBack} />
+				<img src={imgFront} alt="Foreground" className={styles.imgFront} />
 			</div>
-			<div>
+			<div className={styles.content}>
 				<ButtonArrow />
-				<h3>
-					Relax & Rejuvenate <span>Expert Treatments</span>
-				</h3>
-				<ul>
-					<li>
-						<div>
-							<div>
-								<h4>Face Treatments</h4>
-								<div>-----</div>
-								<span>$50</span>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-					</li>
-					<li>
-						<div>
-							<div>
-								<h4>Face Treatments</h4>
-								<div>-----</div>
-								<span>$50</span>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-					</li>
-					<li>
-						<div>
-							<div>
-								<h4>Face Treatments</h4>
-								<div>-----</div>
-								<span>$50</span>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-					</li>
-					<li>
-						<div>
-							<div>
-								<h4>Face Treatments</h4>
-								<div>-----</div>
-								<span>$50</span>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-					</li>
-					<li>
-						<div>
-							<div>
-								<h4>Face Treatments</h4>
-								<div>-----</div>
-								<span>$50</span>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-					</li>
-				</ul>
-				<ButtonMain />
+				<div className={styles.pricingBox}>
+					<h3 className={styles.title}>
+						Relax & Rejuvenate <span>Expert Spa Treatments</span>
+					</h3>
+					<img src={servicesBackground} alt="Background" />
+					<ul className={styles.pricingList}>
+						{services.map((service, index) => (
+							<li key={index} className={styles.serviceItem}>
+								<div className={styles.serviceHeader}>
+									<h4>{service.title}</h4>
+									<div className={styles.price}>
+										<div className={styles.separator}></div>
+										<span>${service.price}</span>
+									</div>
+								</div>
+								<p>{service.desc}</p>
+							</li>
+						))}
+					</ul>
+					<ButtonMain />
+				</div>
 			</div>
 		</section>
 	);
