3eaa118eeed8b9452742089add1982a2287cf12e
patryk-o-dev <patryk.o.dev@gmail.com>
2026-01-18 21:25:29 +0100

    style: SpecialOffers

--- src/components/layout/SpecialOffers/SpecialOffers.tsx
+++ src/components/layout/SpecialOffers/SpecialOffers.tsx
@@ -1,16 +1,27 @@
 import ButtonMain from "../../utils/ButtonMain/ButtonMain";
+import styles from "./SpecialOffers.module.scss";
+import backgroundVideo from "../../../assets/videos/specialOfferBackground.mp4";
+import specialOfferImage from "../../../assets/images/specialOffers/specialOffer.png";
 
 const SpecialOffers = () => {
 	return (
-		<section>
-			<video src="#"></video>
-			<div>
-				<img src="" alt="" />
-				<div>
+		<section className={styles.specialOffers}>
+			<div className={styles.videoContainer}>
+				<div className={styles.overlay}></div>
+				<video
+					className={styles.video}
+					src={backgroundVideo}
+					autoPlay
+					loop
+					muted
+				></video>
+			</div>
+			<div className={styles.content}>
+				<img src={specialOfferImage} alt="Special Offer" />
+				<div className={styles.textContent}>
 					<h3>Special Offers</h3>
 					<p>
-						20% off your first visit , Free consultation with selected
-						treatments
+						20% off your first visit, Free consultation with selected	treatments
 					</p>
 					<ButtonMain />
 				</div>
